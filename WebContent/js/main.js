402a3ae8eafd45aafbe6281ee93712231bfae1b3
Ido Orlov <ido172@gmail.com>
2014-11-01 20:52:46 +0200

    dfgdf

--- WebContent/js/main.js
+++ WebContent/js/main.js
@@ -3,9 +3,9 @@
  * To change this template file, choose Tools | Templates
  * and open the template in the editor.
  */
-var kidTemplate = '<img src="[IMG_SRC]" class="profile_picture v_align background_cover"/>'+
-                  '<div class="profile_name v_align">[NAME_OF_CHILD]</div>';
-         
+var kidTemplate = '<img src="[IMG_SRC]" class="profile_picture v_align background_cover"/>' +
+        '<div class="profile_name v_align">[NAME_OF_CHILD]</div>';
+
 var UID = -1;
 
 function getCookie(cname) {
@@ -38,15 +38,25 @@ function getChildren() {
     });
 }
 
+function fixElementsApperance() {
+    $(".profile_picture").width($(".profile_picture").height());
+    $(".burger_btn").width($(".burger_btn").height() * 1.91);
+    $(".burger_btn").css("margin-top", "-" + ($(".burger_btn").height() / 2) + "px");
+    $(".profile_picture").css("margin-top", "-" + ($(".profile_picture").height() / 2) + "px");
+    $(".profile_name").css("margin-top", "-" + ($(".profile_name").height() / 2) + "px");
+    $(".profile_name").css("right", (Math.ceil($(".profile_picture").width() * 1.7)) + "px");
+
+}
+
 function addChildrenToPage(resData) {
     alert("add children \n" + resData);
     var data = JSON.parse(resData);
     var gardenName = data.name;
-    document.getElementById("header").innerHTML = gardenName ;
+    document.getElementById("header").innerHTML = gardenName;
     var allChildern = data.data.arrayValues;
     var mainDiv = document.getElementById("mainContent");
-    
-    for(var i = 0; i < allChildern.length; i++) {
+
+    for (var i = 0; i < allChildern.length; i++) {
         var newChildDiv = document.createElement("div");
         newChildDiv.className = "kid_header";
         newChildDiv.onclick = getKidFunction(allChildern[i].kidId, allChildern[i].kidName, allChildern[i].imageLink);
@@ -54,20 +64,20 @@ function addChildrenToPage(resData) {
         innerContant = innerContant.replace('[IMG_SRC]', SERVER_URL + '/images/' + allChildern[i].imageLink);
         newChildDiv.innerHTML = innerContant;
         mainDiv.appendChild(newChildDiv);
-        
+
         var spacer = document.createElement("div");
         spacer.className = "spacer";
         spacer.innerHTML = "&nbsp;";
         mainDiv.appendChild(spacer);
     }
-    
+
     fixElementsApperance();
     setTimeout("fixElementsApperance();", 2500);
 }
 
-function getKidFunction(id,name,img) {
+function getKidFunction(id, name, img) {
     return function() {
-        window.location = SERVER_URL + "/child.html?id=" + id +"&name=" + name + "&img=" + img;
+        window.location = SERVER_URL + "/child.html?id=" + id + "&name=" + name + "&img=" + img;
     };
 }
 
@@ -75,8 +85,8 @@ UID = getCookie("userid");
 if (UID < 0 || UID == "" || !UID) {
     alert("ERROR no UID");
 } else {
-   getChildren();
-    
+    getChildren();
+
     $(document).ready(function() {
         $("#my-menu").mmenu();
         document.body.style = "height: " + $(document).height();
@@ -85,15 +95,7 @@ if (UID < 0 || UID == "" || !UID) {
 
 
 
-        function fixElementsApperance() {
-            $(".profile_picture").width($(".profile_picture").height());
-            $(".burger_btn").width($(".burger_btn").height() * 1.91);
-            $(".burger_btn").css("margin-top", "-" + ($(".burger_btn").height() / 2) + "px");
-            $(".profile_picture").css("margin-top", "-" + ($(".profile_picture").height() / 2) + "px");
-            $(".profile_name").css("margin-top", "-" + ($(".profile_name").height() / 2) + "px");
-            $(".profile_name").css("right", (Math.ceil($(".profile_picture").width() * 1.7)) + "px");
 
-        }
 
         fixElementsApperance();
 
@@ -101,5 +103,7 @@ if (UID < 0 || UID == "" || !UID) {
     });
 
 }
+
+
 
 
