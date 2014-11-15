366e9e131c4c40af3a9126a67711fe0f301cab8f
Ido Orlov <ido172@gmail.com>
2014-11-15 16:32:04 +0200

    Revert "updated according to last meeting"
    This reverts commit 8017346edfb5729572990f6ef3e865355a7158f2.

--- WebContent/js/stats.js
+++ WebContent/js/stats.js
@@ -4,7 +4,29 @@
  * and open the template in the editor.
  */
 
-google.load("visualization", "1", {packages: ["corechart"]});
+
+var QueryString = function() {
+    // This function is anonymous, is executed immediately and 
+    // the return value is assigned to QueryString!
+    var query_string = {};
+    var query = window.location.search.substring(1);
+    var vars = query.split("&");
+    for (var i = 0; i < vars.length; i++) {
+        var pair = vars[i].split("=");
+        // If first entry with this name
+        if (typeof query_string[pair[0]] === "undefined") {
+            query_string[pair[0]] = pair[1];
+            // If second entry with this name
+        } else if (typeof query_string[pair[0]] === "string") {
+            var arr = [query_string[pair[0]], pair[1]];
+            query_string[pair[0]] = arr;
+            // If third or later entry with this name
+        } else {
+            query_string[pair[0]].push(pair[1]);
+        }
+    }
+    return query_string;
+}();
 
 function getChildren() {
     var data = {
@@ -26,7 +48,6 @@ function getChildren() {
 function fixElementsApperance() {
     $(".profile_picture").width($(".profile_picture").height());
     $(".burger_btn").width($(".burger_btn").height() * 1.91);
-    $(".back_btn").css("margin-top", "-" + ($(".back_btn").height() / 2) + "px");
     $(".burger_btn").css("margin-top", "-" + ($(".burger_btn").height() / 2) + "px");
     $(".profile_picture").css("margin-top", "-" + ($(".profile_picture").height() / 2) + "px");
     $(".profile_name").css("margin-top", "-" + ($(".profile_name").height() / 2) + "px");
@@ -34,80 +55,44 @@ function fixElementsApperance() {
 
 }
 
-function setTables() {
-    
-    google.setOnLoadCallback(drawChart);
-    function drawChart() {
-        alert("4");
-        var data = google.visualization.arrayToDataTable([
-            ['הצלחות', 'ימים'],
-            ['2013', 1000],
-            ['2014', 1170],
-            ['2015', 660],
-            ['2016', 1030]
-        ]);
-
-        var options = {
-            title: 'הצלחות',
-            hAxis: {title: 'הצלחות', titleTextStyle: {color: '#333'}},
-            vAxis: {title: 'ימים', titleTextStyle: {color: '#333'}, minValue: 0},
-            'width': $("#mainContent").width() * 0.75,
-            'height': $("#mainContent").height() * 0.65
-        };
-
-        alert("3");
-        var chart1 = new google.visualization.AreaChart(document.getElementById('chart_div1'));
-        var chart2 = new google.visualization.AreaChart(document.getElementById('chart_div2'));
-        chart1.draw(data, options);
-        chart2.draw(data, options);
-    }
-}
-
 
 $(document).ready(function() {
     $("#my-menu").mmenu();
     document.body.style = "height: " + $(document).height();
+    +"px";
     $(window).on('resize', fixElementsApperance);
     $(window).on("orientationchange", fixElementsApperance);
 
     fixElementsApperance();
-    setTables();
-
-
-    //alert(document.getElementById("childName") + ":::" + QueryString.name);
+    
     document.getElementById("childName").innerHTML = decodeURI(QueryString.name);
     document.getElementById("profilePicture").src = "images/" + QueryString.img;
 
-    $("#success").click(function() {
+    $("#success").click(function(){
         if ($("#success_arrow").hasClass("up")) {
             $("#success_arrow").removeClass("up").addClass("down");
         } else {
             $("#success_arrow").removeClass("down").addClass("up");
         }
-        $("#chart_div1").slideToggle();
+      $("#chart_div1").slideToggle();
     });
-
-    $("#times").click(function() {
+    
+    $("#times").click(function(){
         if ($("#times_arrow").hasClass("up")) {
             $("#times_arrow").removeClass("up").addClass("down");
         } else {
             $("#times_arrow").removeClass("down").addClass("up");
         }
-        $("#chart_div2").slideToggle();
+      $("#chart_div2").slideToggle();
     });
-    setTimeout("fixTables();", 1000);
+    setTimeout("fixTables();",1000);
 
-
-
-
-
+    
 });
 
-function fixTables() {
-    $("#chart_div2").hide();
-}
-
-
+    function fixTables() {
+        $("#chart_div2").hide();
+    }
 
 
 
